7b7a4badcd8969c8c6913075983e0d4414885637
PillowPowa <fagodfagod@gmail.com>
2024-06-20 19:36:28 +0300

    feat(todo): todo validation post body

--- internal/api/controllers/todo.go
+++ internal/api/controllers/todo.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"tratodo/internal/domain/models"
 	"tratodo/pkg/api"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
@@ -30,12 +32,46 @@ func (c *TodoController) GetById(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return api.NewApiError(http.StatusBadRequest, "TODO ID should be an integer")
 	}
-	todo, err := c.service.GetById(id)
 
+	todo, err := c.service.GetById(id)
 	if err != nil {
 		return err
 	}
 
 	api.WriteJSON(w, http.StatusOK, todo)
+	return nil
+}
+
+func (c *TodoController) Create(w http.ResponseWriter, r *http.Request) error {
+	todo := new(models.Todo)
+	if err := json.NewDecoder(r.Body).Decode(todo); err != nil {
+		return api.NewApiError(http.StatusBadRequest, "Invalid JSON")
+	}
+
+	if err := validator.New().Struct(todo); err != nil {
+		return api.NewApiError(http.StatusBadRequest, err.Error())
+	}
+
+	t, err := c.service.Create(todo)
+	if err != nil {
+		return err
+	}
+
+	api.WriteJSON(w, http.StatusCreated, t)
+	return nil
+}
+
+func (c *TodoController) Delete(w http.ResponseWriter, r *http.Request) error {
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		return api.NewApiError(http.StatusBadRequest, "TODO ID should be an integer")
+	}
+
+	err = c.service.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	api.WriteJSON(w, http.StatusOK, true)
 	return nil
 }
--- internal/domain/models/todo.go
+++ internal/domain/models/todo.go
@@ -2,6 +2,6 @@ package models
 
 type Todo struct {
 	ID        int    `json:"id"`
-	Title     string `json:"title"`
+	Title     string `json:"title" validate:"required,min=3,max=32"`
 	Completed bool   `json:"completed"`
 }
