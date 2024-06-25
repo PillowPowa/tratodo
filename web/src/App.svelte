005494579c7c9de933df88dd7162dd25062e6b0b
PillowPowa <fagodfagod@gmail.com>
2024-06-25 21:16:43 +0300

    feat(todo): todo card

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { user } from "./api";
-  import Checkbox from "./components/Checkbox.svelte";
+  import Button from "./components/Button.svelte";
+  import TodoCard from "./components/todo/TodoCard.svelte";
+  import UserCard from "./components/user/UserCard.svelte";
+  import Select from "./components/select/Select.svelte";
+  import SelectTrigger from "./components/select/SelectTrigger.svelte";
+  import SelectContent from "./components/select/SelectContent.svelte";
+  import SelectItem from "./components/select/SelectItem.svelte";
 
   onMount(() => {
     user
@@ -43,48 +49,30 @@
   ];
 </script>
 
-<main class="min-h-screen grid place-items-center">
-  <div class="space-y-12 bg-muted max-w-2xl w-full h-[450px] rounded-md">
+<main class="min-h-screen py-12 grid place-items-center">
+  <div class="space-y-4 bg-muted max-w-2xl w-full h-fit rounded-md">
     <h2
-      class="text-6xl uppercase leading-[110%] font-bold text-center text-secondary"
+      class="text-6xl mb-12 uppercase leading-[110%] font-bold text-center text-secondary"
     >
       Todo List
     </h2>
 
+    <div class="flex justify-between gap-x-4 px-2 py-4 bg-secondary rounded-md">
+      <Button variant="primary" size="lg">Add Todo</Button>
+
+      <Select>
+        <SelectTrigger class="min-w-[160px] w-full" />
+        <SelectContent>
+          <SelectItem label="123" value="123" />
+          <SelectItem label="321" value="321" />
+        </SelectContent>
+      </Select>
+    </div>
     <div class="p-6 space-y-4 bg-secondary rounded-lg">
       {#each todosMock as todo (todo.id)}
-        <div
-          data-completed={todo.completed}
-          class="px-2 py-4 bg-background rounded-md data-[completed='true']:opacity-60 transition-colors"
-        >
-          <div class="flex items-center gap-x-2">
-            <Checkbox checked={todo.completed}>
-              <div class="text-foreground/80 font-medium">
-                {#if todo.completed}
-                  <del>{todo.title}</del>
-                {:else}
-                  {todo.title}
-                {/if}
-                <p class="text-xs text-foreground/50">
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  }).format(new Date())}
-                </p>
-              </div>
-            </Checkbox>
-
-            <div class="ml-auto">
-              <button
-                class="px-2 py-1 bg-primary h-7 text-secondary text-sm rounded-md"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+        <TodoCard {todo} />
       {/each}
     </div>
+    <UserCard />
   </div>
 </main>
---
+++ web/src/components/todo/TodoCard.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { Todo } from "src/types/todo";
+  import Button from "../Button.svelte";
+  import Checkbox from "../Checkbox.svelte";
+  import EditIcon from "../icons/EditIcon.svelte";
+  import TrashIcon from "../icons/TrashIcon.svelte";
+
+  export let todo: Todo;
+</script>
+
+<div
+  data-completed={todo.completed}
+  class="px-2 py-4 bg-white/70 backdrop-blur-sm rounded-md data-[completed='true']:opacity-60 transition-colors"
+>
+  <div class="flex items-center gap-x-2">
+    <Checkbox checked={todo.completed}>
+      <div class="text-foreground/80 font-medium">
+        <p class="text-balance line-clamp-5">
+          {#if todo.completed}
+            <del>{todo.title}</del>
+          {:else}
+            {todo.title}
+          {/if}
+        </p>
+        <p class="text-xs text-foreground/50">
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }).format(new Date())}
+        </p>
+      </div>
+    </Checkbox>
+
+    <div class="ml-auto flex items-center gap-x-2">
+      <Button aria-label="Edit todo" variant="secondary" size="icon">
+        <EditIcon />
+      </Button>
+
+      <Button aria-label="Delete todo" variant="destructive" size="icon">
+        <TrashIcon />
+      </Button>
+    </div>
+  </div>
+</div>
