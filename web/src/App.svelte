65c4d475465119874b214336b78d1490ea1b0f40
PillowPowa <fagodfagod@gmail.com>
2024-06-26 20:41:10 +0300

    feat: better home makrup

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { user } from "./api";
-  import Button from "./components/Button.svelte";
   import TodoCard from "./components/todo/TodoCard.svelte";
   import UserCard from "./components/user/UserCard.svelte";
   import Select from "./components/select/Select.svelte";
   import SelectTrigger from "./components/select/SelectTrigger.svelte";
   import SelectContent from "./components/select/SelectContent.svelte";
   import SelectItem from "./components/select/SelectItem.svelte";
+  import EditableTodoCard from "./components/todo/EditableTodoCard.svelte";
 
   onMount(() => {
     user
@@ -49,30 +49,32 @@
   ];
 </script>
 
-<main class="min-h-screen py-12 grid place-items-center">
+<main class="min-h-screen grid place-items-center">
   <div class="space-y-4 bg-muted max-w-2xl w-full h-fit rounded-md">
     <h2
-      class="text-6xl mb-12 uppercase leading-[110%] font-bold text-center text-secondary"
+      class="text-6xl my-6 uppercase leading-[110%] font-bold text-center text-secondary"
     >
       Todo List
     </h2>
 
     <div class="flex justify-between gap-x-4 px-2 py-4 bg-secondary rounded-md">
-      <Button variant="primary" size="lg">Add Todo</Button>
+      <UserCard />
 
       <Select>
-        <SelectTrigger class="min-w-[160px] w-full" />
+        <SelectTrigger class="min-w-[160px] w-full" placeholder="All" />
         <SelectContent>
-          <SelectItem label="123" value="123" />
-          <SelectItem label="321" value="321" />
+          <SelectItem label="Completed" value="completed" />
+          <SelectItem label="Uncompleted" value="uncompleted" />
         </SelectContent>
       </Select>
     </div>
-    <div class="p-6 space-y-4 bg-secondary rounded-lg">
+    <div
+      class="px-2 py-4 sm:p-6 space-y-4 bg-secondary rounded-lg max-h-[550px] overflow-auto"
+    >
+      <EditableTodoCard />
       {#each todosMock as todo (todo.id)}
         <TodoCard {todo} />
       {/each}
     </div>
-    <UserCard />
   </div>
 </main>
