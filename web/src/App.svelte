a7ab74cc41126436d2b7ed0a567d19ab52c4a4b1
PillowPowa <fagodfagod@gmail.com>
2024-06-29 18:25:48 +0300

    feat(auth): protected route

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { Route, Router } from "svelte-routing";
   import Home from "./routes/Home.svelte";
+  import ProtectedRoute from "./components/ProtectedRoute.svelte";
 
   export let url = "";
 </script>
 
 <Router {url}>
-  <Route path="/" component={Home} />
+  <ProtectedRoute path="/" component={Home} />
+  <Route path="/login"></Route>
 </Router>
---
+++ web/src/components/ProtectedRoute.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { userStore } from "../store/user";
+  import type { SvelteComponent } from "svelte";
+  import { navigate, Route } from "svelte-routing";
+  import { get } from "svelte/store";
+
+  const userState = get(userStore);
+  if (!userState.user && userState.status === "unauthenticated") {
+    navigate("/login");
+  } else if (userState.status === "initial") {
+    userStore.fetchUser().catch(() => {
+      navigate("/login");
+    });
+  }
+
+  export let path;
+  export let component: typeof SvelteComponent<any>;
+</script>
+
+<Route {path} let:params>
+  {#if $userStore.status === "initial"}
+    <div class="min-h-screen bg-black grid place-items-center"></div>
+  {:else if $userStore.user}
+    <svelte:component this={component} {...params} />
+  {/if}
+</Route>
